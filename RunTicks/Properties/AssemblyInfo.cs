53e8331cb0c975e9d23d617e909e2d021aa71950
Khanlar Mammadov <khanlar@mammadov.pro>
2020-06-26 23:13:43 +0400

    CLSCompliant attribute added to the assembly.

--- RunTicks/Properties/AssemblyInfo.cs
+++ RunTicks/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
+using System;
 
+[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("RunTicks.UnitTests")]
 [assembly: InternalsVisibleTo("RunTicks.IntegrationTests")] 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
