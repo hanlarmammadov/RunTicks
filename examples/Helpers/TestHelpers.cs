a2133025f32ea5a0b8e68fa6538c9c232be41db6
Khanlar Mammadov <khanlar@mammadov.pro>
2020-06-27 17:19:30 +0400

    A project with examples added to the repository.

---
+++ examples/Helpers/TestHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examples.Helpers
+{
+   public static class TestHelpers
+    {
+        public static void SomeAction()
+        {
+            Thread.Sleep(10);
+        }
+        public static Task SomeAsyncAction()
+        {
+            return Task.CompletedTask;
+        }
+        public static void SomeInitialization()
+        {
+
+        }
+        public static void SomeCleanUp()
+        {
+
+        }
+    }
+}
