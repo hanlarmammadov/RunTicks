b9e8139a7d2809871de8c4c7324e6af396649e98
Khanlar Mammadov <khanlar@mammadov.pro>
2020-06-27 15:25:52 +0400

    Some docs added. (#1)
    * Code documented.
    
    * Code documented.

--- RunTicks/Measurement.cs
+++ RunTicks/Measurement.cs
@@ -6,6 +6,9 @@ using System.Threading;
 
 namespace RunTicks
 {
+    /// <summary>
+    /// Main class which is a starting point for creating and running the measurements. 
+    /// </summary>
     public class Measurement
     {
         private string _measurementName;
@@ -25,56 +28,111 @@ namespace RunTicks
         internal Action PreAction => _preAction;
         internal Action PostAction => _postAction;
 
+        /// <summary>
+        /// Starting point for building a measurement using builder methods.
+        /// </summary>
+        /// <param name="name">Measurement name.</param>
+        /// <returns>Created measurement.</returns>
         public static Measurement Create(string name)
         {
             return new Measurement(name);
         }
+        /// <summary>
+        /// Starting point for building a measurement using builder methods.
+        /// </summary>
+        /// <returns>Created measurement.</returns>
         public static Measurement Create()
         {
             return new Measurement(null);
         }
+        /// <summary>
+        /// Adds a parameterless action that is being measured.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>Self.</returns>
         public Measurement OfAction(Action action)
         {
             ValidateActionAndMeasurement(action);
             _measurementStrategy = new AutoStopwatchStrategy(action);
             return this;
         }
+        /// <summary>
+        /// Adds a measured action taking an instance of <c>System.Diagnostics.Stopwatch</c>.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>Self.</returns>
         public Measurement OfAction(Action<Stopwatch> action)
         {
             ValidateActionAndMeasurement(action);
             _measurementStrategy = new ManualStopwatchStrategy(action);
             return this;
         }
+        /// <summary>
+        /// Adds a measured async parameterless action that is being measured.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>Self.</returns>
         public Measurement OfAsyncAction(AsyncAction action)
         {
             ValidateActionAndMeasurement(action);
             _measurementStrategy = new AsyncAutoStopwatchStrategy(action);
             return this;
         }
+        /// <summary>
+        /// Adds a measured async action taking an instance of <c>System.Diagnostics.Stopwatch</c>.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>Self.</returns>
         public Measurement OfAsyncAction(AsyncActionWithStopwatch action)
         {
             ValidateActionAndMeasurement(action);
             _measurementStrategy = new AsyncManualStopwatchStrategy(action);
             return this;
         }
+        /// <summary>
+        /// Specifies a pre-action that is executed before every iteration of execution of the measured action.
+        /// The execution time of pre-action is never considered.
+        /// </summary>
+        /// <param name="preAction"></param>
+        /// <returns>Self.</returns>
         public Measurement WithPreAction(Action preAction)
         {
-
-            _preAction = preAction??throw new ArgumentNullException("preAction");
+            _preAction = preAction ?? throw new ArgumentNullException("preAction");
             return this;
         }
+        /// <summary>
+        /// Specifies a post-action that is executed after every iteration of execution of the measured action.
+        /// The execution time of post-action is never considered.
+        /// </summary>
+        /// <param name="postAction"></param>
+        /// <returns>Self.</returns>
         public Measurement WithPostAction(Action postAction)
         {
             _postAction = postAction ?? throw new ArgumentNullException("postAction");
             return this;
         }
+        /// <summary>
+        /// Specifies some custom data that will be added to the result of this measurement.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns>Self.</returns>
         public Measurement WithData(string name, object value)
         {
+            if (name == null)
+                throw new ArgumentNullException("name");
             if (_additionalData == null)
                 _additionalData = new Dictionary<string, object>();
             _additionalData.Add(name, value);
             return this;
         }
+        /// <summary>
+        /// Performs the time measurement.
+        /// </summary>
+        /// <param name="numberOfTimes">Specifies the number of times the measured action should be called during the measurement.</param>
+        /// <param name="attemptName">Name of the current attempt.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Result of the measurement containing the measured metrics.</returns>
         public RunResult Run(Int64 numberOfTimes, string attemptName = null, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
@@ -82,12 +140,12 @@ namespace RunTicks
             if (_measurementStrategy == null)
                 throw new InvalidOperationException("No action was provided to conduct a measurement. Please specify the action.");
 
-            // Stopwatch
-                Stopwatch stopwatch = new Stopwatch();
+            // Stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
             Int64 elapsedTicks = 0;
             DateTime startDate = DateTime.Now;
 
-            // Execute action
+            // Execute action.
             for (int i = 1; i <= numberOfTimes; i++)
             {
                 cancellationToken.ThrowIfCancellationRequested();
@@ -107,10 +165,23 @@ namespace RunTicks
             DateTime endDate = DateTime.Now;
             return new RunResult(elapsedTicks, numberOfTimes, startDate, endDate, _measurementName, attemptName, _additionalData);
         }
+        /// <summary>
+        /// Performs the time measurement.
+        /// </summary>
+        /// <param name="numberOfTimes">Specifies the number of times the measured action should be called during the measurement.</param>
+        /// <param name="attemptName">Name of the current attempt.</param>
+        /// <param name="timeout">Specifies the timeout in milliseconds after which the measurement terminates with a <c>System.OperationCanceledException</c> exception.</param>
+        /// <returns>Result of the measurement containing the measured metrics.</returns>
         public RunResult Run(Int64 numberOfTimes, string attemptName, int timeout)
         {
             return Run(numberOfTimes, attemptName, new CancellationTokenSource(timeout).Token);
         }
+        /// <summary>
+        /// Performs the time measurement.
+        /// </summary>
+        /// <param name="numberOfTimes">Specifies the number of times the measured action should be called during the measurement.</param>
+        /// <param name="timeout">Specifies the timeout in milliseconds after which the measurement terminates with a <c>System.OperationCanceledException</c> exception.</param>
+        /// <returns>Result of the measurement containing the measured metrics.</returns>
         public RunResult Run(Int64 numberOfTimes, int timeout)
         {
             return Run(numberOfTimes, null, new CancellationTokenSource(timeout).Token);
--- RunTicks/RunResult.cs
+++ RunTicks/RunResult.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace RunTicks
 {
+    /// <summary>
+    /// Represents the measured metrics as a result of a conducted measurement.
+    /// </summary>
     public class RunResult
     {
         private readonly long _numberOfRuns;
@@ -14,13 +17,23 @@ namespace RunTicks
         private readonly Dictionary<string, object> _additionalData;
         private readonly AssertPart _assertPart;
 
-        public RunResult(Int64 elapsedTicks,
-                         Int64 numberOfRuns,
-                         DateTime startDate,
-                         DateTime endDate,
-                         string measurementName,
-                         string attemptName,
-                         Dictionary<string, object> additionalData = null)
+        /// <summary>
+        /// Creates an instance of <c>RunTicks.RunResult</c> using the provided parameters.
+        /// </summary>
+        /// <param name="elapsedTicks">Total ticks elapsed for this attempt of the measurement.</param>
+        /// <param name="numberOfRuns">Number of runs of measuring action method.</param>
+        /// <param name="startDate">Start date of the measurement.</param>
+        /// <param name="endDate">End date of the measurement.</param>
+        /// <param name="measurementName">Name of the measurement.</param>
+        /// <param name="attemptName">Name of the current attempt.</param>
+        /// <param name="additionalData">Optional user provided data related to the measurement.</param>
+        internal RunResult(Int64 elapsedTicks,
+                           Int64 numberOfRuns,
+                           DateTime startDate,
+                           DateTime endDate,
+                           string measurementName,
+                           string attemptName,
+                           Dictionary<string, object> additionalData = null)
         {
             _elapsedTime = new TimeSpan(elapsedTicks);
             _numberOfRuns = numberOfRuns;
@@ -31,25 +44,77 @@ namespace RunTicks
             _additionalData = additionalData;
             _assertPart = new AssertPart(this);
         }
-        public TimeSpan ElapsedTime => _elapsedTime; 
+
+        /// <summary>
+        /// Number of runs of measuring action method.
+        /// </summary>
         public long NumberOfRuns => _numberOfRuns;
-        // Total values.
-        public Int64 TotalElapsedTicks => _elapsedTime.Ticks; 
+        /// <summary>
+        /// Total time elapsed for this attempt of the measurement.
+        /// </summary>
+        public TimeSpan ElapsedTime => _elapsedTime;
+        /// <summary>
+        /// Total ticks elapsed for this attempt of the measurement.
+        /// 1 millisecond = 10 000 ticks.
+        /// </summary>
+        public Int64 TotalElapsedTicks => _elapsedTime.Ticks;
+        /// <summary>
+        /// Total milliseconds elapsed for this attempt of the measurement.
+        /// </summary>
         public Double TotalMilliseconds => _elapsedTime.TotalMilliseconds;
+        /// <summary>
+        /// Total seconds elapsed for this attempt of the measurement.
+        /// </summary>
         public Double TotalSeconds => _elapsedTime.TotalSeconds;
+        /// <summary>
+        /// Total minutes elapsed for this attempt of the measurement.
+        /// </summary>
         public Double TotalMinutes => _elapsedTime.TotalMinutes;
-        // Average values.
+         
+        /// <summary>
+        /// Average ticks elapsed for a single action run.
+        /// Averages are counted as totals divides number of runs.
+        /// </summary>
         public Double AverageTicks => ((Double)_elapsedTime.Ticks) / _numberOfRuns;
+        /// <summary>
+        /// Average milliseconds elapsed for a single action run.
+        /// Averages are counted as totals divides number of runs.
+        /// </summary>
         public Double AverageMilliseconds => ((Double)_elapsedTime.TotalMilliseconds) / _numberOfRuns;
+        /// <summary>
+        /// Average seconds elapsed for a single action run.
+        /// Averages are counted as totals divides number of runs.
+        /// </summary>
         public Double AverageSeconds => ((Double)_elapsedTime.TotalSeconds) / _numberOfRuns;
+        /// <summary>
+        /// Average minutes elapsed for a single action run.
+        /// Averages are counted as totals divides number of runs.
+        /// </summary>
         public Double AverageMinutes => ((Double)_elapsedTime.TotalMinutes) / _numberOfRuns;
-        // Other data.
+
+        /// <summary>
+        /// Name of the measurement.
+        /// </summary>
         public string MeasurementName => _measurementName;
+        /// <summary>
+        /// Name of the current attempt.
+        /// </summary>
         public string AttemptName => _attemptName;
+        /// <summary>
+        /// Start date of the measurement.
+        /// </summary>
         public DateTime StartDate => _startDate;
+        /// <summary>
+        /// End date of the measurement.
+        /// </summary>
         public DateTime EndDate => _endDate;
+        /// <summary>
+        /// Optional user provided data related to the measurement.
+        /// </summary>
         public Dictionary<string, object> AdditionalData => _additionalData;
-        // Assert part.
+        /// <summary>
+        /// Test assertions for this measurement result.
+        /// </summary>
         public AssertPart Should => _assertPart;
     }
 }
